174dcda1599dd7f464853715441895c4fd99f425
Khayyam Saleem <khayyam.saleem@gmail.com>
2016-10-18 17:25:58 -0400

    initial commit

---
+++ auto.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -n "Which homework? "
+read HW
+
+s=$(cat ../students.txt| awk '{ print $2 $1 }' | tr 'A-Z' 'a-z')
+
+for x in $s
+do
+	PY=(../hw$HW/$x*.py)
+	cp "$PY" .
+	MYPY=($x*.py)
+	echo >> $MYPY
+	cat script_hw$HW.py >> $MYPY
+	echo running $MYPY "($x)"
+	python3 "$MYPY"
+	echo -e "\n\n\n=======================================\n\n\n"
+done
